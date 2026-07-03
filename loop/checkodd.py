4c7d70cbbc4885c5948ede9c1f17958a63003016
sharan-zona <169944147+sharan-zona@users.noreply.github.com>
2026-07-03 11:37:25 +0530

    change

--- loop/checkodd.py
+++ loop/checkodd.py
@@ -1,8 +1,6 @@
 print("-----Printing odd numbers upto a given input range-----")
 userin = int(input("Enter a Number: "))
-
-for i in range(1,userin):
-    if i%2 == 0:
-        print(i,"Even")
-    else:
-        print(i,"Odd")
+for i in range(0,userin,2):
+    print(i,"Even")
+for i in range(1,userin,2):
+    print(i,"Odd")
